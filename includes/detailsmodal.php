ac9ff0b98f5d469d1004e92a5c4acda206513f50
Mateusz Mazur <mateuszmaz@interia.pl>
2017-10-06 16:35:42 +0200

    Add files via upload

--- includes/detailsmodal.php
+++ includes/detailsmodal.php
@@ -1,39 +1,57 @@
+<?php
+require_once '../core/init.php';
+$id = $_POST['id'];
+$id = (int)$id;
+$sql = "SELECT * FROM products WHERE id = '$id'";
+$result = $db->query($sql);
+$product = mysqli_fetch_assoc($result);
+$brand_id = $product['brand'];
+$sql = "SELECT brand FROM brand WHERE id = '$brand_id'";
+$brand_query = $db->query($sql);
+$brand = mysqli_fetch_assoc($brand_query);
+$sizestring = $product['sizes'];
+$size_array = explode(',', $sizestring);
+?>
+
 <!-- Details Light Box Modal -->
-	
-	<div class="modal fade details-1" id="details-1" tabindex="-1" aria-labelledby="#details-1">
+	<?php ob_start(); ?>
+	<div class="modal fade details-1" id="details-modal" tabindex="-1" aria-labelledby="#details-1">
 		<div class="modal-dialog modal-lg">
 			<div class="modal-content">
 			<div class="modal-header">
-				<button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title text-center">Levis Jeans</h4>
+				<button class="close" type="button" onclick="closeModal()" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title text-center"><?= $product['title']; ?></h4>
 			</div>
 				<div class="modal-body">
 					<div class="container-fluid">
 						<div class="row">
 							<div class="col-sm-6">
-								<div class="center-block"><img src="images/products/men4.png" alt="Levis Jeans" class="details img-responsive"></div>
+								<div class="center-block"><img src="<?= $product['image']; ?>" alt="<?= $product['title']; ?>" class="details img-responsive"></div>
 							</div>
 							<div class="col-sm-6">
 								<h4>Details</h4>
-								<p>DescriptionThese jeans are amazing! They are straight leg, fit great and looks gorgeous.</p>
+								<p><?= $product['description']; ?></p>
 								<hr>
-								<p>Price: $34.99</p>
-								<p>Brand: Levis</p>
+								<p>Price: $<?= $product['price']; ?></p>
+								<p>Brand: <?= $brand['brand']; ?></p>
 								<form action="add_cart.php" method="post">
 									<div class="form-group">
 										<div class="col-xs-3">
 											<label for="quantity">Quantity:</label>
 											<input type="text" class="form-control" id="quantity" name="quantity">
 										</div><div class="col-xs-9"></div>
-										<p>Available: 3</p>
+										
 									</div><br><br>
 									<div class="form-group">
 										<label for="size">Size:</label>
 										<select name="size" id="size" class ="form-control">
 											<option value=""></option>
-											<option value="28">28</option>
-											<option value="32">32</option>
-											<option value="36">36</option>
+											<?php foreach($size_array as $string){
+												$string_array = explode(':', $string);
+												$size = $string_array[0];
+												$quantity = $string_array[1];
+												echo '<option value="'.$size.'">'.$size.' ('.$quantity.' Available)</option>';
+											}?>
 										</select>
 									</div>
 								</form>
@@ -43,9 +61,20 @@
 				</div>
 	
 				<div class="modal-footer">
-					<button class="btn btn-default" data-dismiss="modal">Close</button>
+					<button class="btn btn-default" onclick="closeModal()" >Close</button>
 					<button class="btn brn-warning" type="submit"><span class="glyphicon glyphicon-shopping-cart"></span>Add to cart</button>
 				</div>
 			</div>
 		</div>
 	</div>
+	
+	<script>
+		function closeModal(){
+			$('#details-modal').modal('hide');
+			setTimeout(function(){
+				$('#details-modal').remove();
+				$('.modal-backdrop').remove();
+			},500);
+		}
+	</script>
+	<?php echo ob_get_clean(); ?>
--- includes/footer.php
+++ includes/footer.php
@@ -22,6 +22,23 @@
 		  });
 		});
 
+		
+		function detailsmodal(id){
+			var data = {"id" : id};
+			$.ajax({
+				url : <?=BASEURL;?>+'includes/detailsmodal.php',
+				method : "post",
+				data : data,
+				success: function(data){
+					$('body').append(data);
+					$('#details-modal').modal('toggle');
+				},
+				error: function(){
+					alert("Something went wrong!");
+				}
+			});
+			
+		}
 	</script>
 </body>
 </html>

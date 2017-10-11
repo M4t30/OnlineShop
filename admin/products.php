cc2e7a0865aa84b7a38fd70a399c303dda36f47a
Mateusz Mazur <mateuszmaz@interia.pl>
2017-10-11 23:37:00 +0200

    Add files via upload

--- admin/products.php
+++ admin/products.php
@@ -2,6 +2,71 @@
 require_once $_SERVER['DOCUMENT_ROOT'].'/onlineShop/core/init.php';
 include 'includes/head.php';
 include 'includes/navigation.php';
+
+if(isset($_GET['add'])){
+	$brandQuery = $db->query("SELECT * FROM brand ORDER BY brand");
+	$parentQuery = $db->query("SELECT * FROM categories WHERE parent = 0 ORDER BY category");
+	?> 
+	<h2 class="text-center">Add a New Product</h2><hr>
+	<form action="products.php?add=1" method="POST" enctype="multipart/form-data">
+		<div class="form-group col-md-3">
+			<label for="title">Title*:</label>
+			<input type="text" name="title" class="form-control" id="title" value="<?=((isset($_POST['title']))?sanitize($_POST['title']):'');?>">
+		</div>
+		<div class="form-group col-md-3">
+			<label for="brand">Brand*:</label>
+			<select class="form-control" id="brand" name="brand">
+				<option value=""<?=((isset($_POST['brand']) && $_POST['brand'] == '')? ' selected':'')?></option>
+				<?php while($brand = mysqli_fetch_assoc($brandQuery)): ?>
+					<option value="<?=$brand['id'];?>"<?=((isset($_POST['brand']) && $_POST['brand'] == $brand['id'])?' selected':'');?>><?=$brand['brand'];?></option>
+				<?php endwhile; ?>
+			</select>
+		</div>
+		<div class="form-group col-md-3">
+			<label for="parent">Parent Category*:</label>
+			<select class="form-control" id="parent" name="parent">
+			<option value=""<?=((isset($_POST['parent']) && $_POST['parent'] == '')?' selected':'');?>></option>
+				<?php while($parent = mysqli_fetch_assoc($parentQuery)): ?>
+					<option value="<?=$parent['id'];?>"<?=((isset($_POST['parent']) && $_POST['parent'] == $parent['id'])?' select':'');?>><?=$parent['category'];?></option>
+				<?php endwhile; ?>
+			</select>
+			</label>
+		</div>
+		<div class="form-group col-md-3">
+			<label for="child">Child Category*:</label>
+			<select id="child" name="child" class="form-control">
+			</select>
+		</div>		
+		<div class="form-group col-md-3">
+			<label for="price">Price*:</label>
+			<input type="text" id="price" name="price" class="form-control" value="<?=((isset($_POST['price']))?sanitize($_POST['price']):''); ?>">
+		</div>
+		<div class="form-group col-md-3">
+			<label for="price">List price*:</label>
+			<input type="text" id="price" name="list_price" class="form-control" value="<?=((isset($_POST['list_price']))?sanitize($_POST['list_price']):''); ?>">
+		</div>
+		<div class="form-group col-md-3">
+			<label>Quantity & Sizes*:</label>
+			<button class="btn btn-default form-control" onclick="$('#sizesModal').modal('toggle');return false;">Quantity & Sizes</button>
+		</div>
+		<div class="form-group col-md-3">
+			<label for="sizes">Sizes & Quantity Preview</label>
+			<input type="text" class="form-control" name="size" id="size" value="<?=((isset($_POST['sizes']))?$_POST['sizes']:''); ?>" readonly>
+		</div>
+		<div class="form-group col-md-6">
+			<label for="photo">Product Photo:</label>
+			<input type="file" name="photo" id="photo" class="form-control">
+		</div>
+		<div class="form-group col-md-6">
+			<label for="description">Description:</label>
+			<textarea id="description" name="description" class="form-control" rows="6"><?=((isset($_POST['description']))?sanitize($_POST['description']):''); ?></textarea>
+		</div>
+		<div class="form-group pull-right">
+		<input type="submit" value="Add Product" class="form-control btn btn-success">
+		</div><div class="clearfix"></div>
+	</form>
+<?php } else{
+	
 $sql = "SELECT * FROM products WHERE deleted = 0";
 $presults = $db->query($sql);
 if(isset($_GET['featured'])){
@@ -48,4 +113,6 @@ if(isset($_GET['featured'])){
 	</tbody>
 </table>
 
-<?php include 'includes/footer.php';
+<?php } 
+
+include 'includes/footer.php';

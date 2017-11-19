40e1a3b154e3a00275eb3499a056e7a5c0fd3516
Mateusz Mazur <mateuszmaz@interia.pl>
2017-11-19 12:32:27 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -2,3 +2,4 @@
 	define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/onlineShop/');
 	define('CART_COOKIE', 'SBwi72UCklwiqzz2');
 	define('CART_COOKIE_EXPIRE', time() + (86400 * 30));
+	define('TAXRATE', 0.087);
